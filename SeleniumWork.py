54f098c4eede2b2641168db00e5916940d570536
yogymax <yogymax@gmail.com>
2019-02-02 16:26:13 +0530

    first commit

---
+++ SeleniumWork.py
@@ -0,0 +1,40 @@
+from selenium import webdriver as browser
+
+
+chrom = browser.Chrome
+chrom.find_elements_by_css_selector('')
+#id,name,tagname,css,classname,linktext,partiallinktext,xpath
+#id -- within a page developer keeps unique -- so find_by_id -- returns single webelement
+#name -- when page has same category of elements -- gender/yearofexpr
+#linktext/partialLinkText --
+#tagname -- mostly dynamic tables
+#className --its a css template..to apply/reuse for multiple elements of same kind
+#css --
+
+
+
+
+
+
+
+#elements -- always returns listofwebelements --
+#element -- always returns of webelement
+#
+
+'''
+Select(webelement) - - selenium - - Dropdown
+select
+sathi
+use
+krto
+Actions(webdriver) - -- selenium - - MouseHover
+actions
+sathi
+use
+krtat - drag / drop
+double
+click - -
+'''
+
+
+
